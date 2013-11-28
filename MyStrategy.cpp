d55e82281694d0a86e83a53a66d1aa941b785128
slava.sh <shk.slava@gmail.com>
2013-11-28 17:28:52 +0600

    Empty

--- MyStrategy.cpp
+++ MyStrategy.cpp
@@ -22,7 +22,6 @@ typedef vector< vector< CellType > > Cells;
 const int inf = 1e9;
 int sizeX;
 int sizeY;
-int move_index = 0;
 
 struct Point {
     int x, y;
@@ -145,135 +144,51 @@ struct Dijkstra {
     }
 };
 
+struct SlavaStrategy {
+
+    Point target;
+    int move_index = 0;
+
+    void move(const Trooper& self,
+            const World& world, const Game& game, Move& action) {
+        move_index += 1;
+
+        if (move_index == 1) {
+            sizeX = world.getWidth();
+            sizeY = world.getHeight();
+        }
+
+        Point pos(self);
+        int action_points = self.getActionPoints();
+
+        logId("move number " << move_index);
+        logId(self.getType() << " (" << action_points << ") at " << pos);
+
+        auto& bonuses = world.getBonuses();
+        auto& troopers = world.getTroopers();
+        auto cells = world.getCells();
+        vector< Trooper > teammates;
+        vector< Trooper > enemies;
+        for (auto& trooper : troopers) {
+            if (trooper.isTeammate()) {
+                cells[trooper.getX()][trooper.getY()] = HIGH_COVER;
+                teammates.push_back(trooper);
+            }
+            else {
+                enemies.push_back(trooper);
+            }
+        }
+        logId("we see " << enemies.size() << " enemies");
+
+        Dijkstra dijkstra(pos, cells);
+    }
+};
+
 MyStrategy::MyStrategy() {
 }
 
-Point target;
-
 void MyStrategy::move(const Trooper& self,
         const World& world, const Game& game, Move& action) {
-
-    move_index += 1;
-
-    if (move_index == 1) {
-        sizeX = world.getWidth();
-        sizeY = world.getHeight();
-    }
-
-    Point pos(self);
-    int action_points = self.getActionPoints();
-    logId("move number " << move_index);
-    logId(self.getType() << " (" << action_points << ") at " << pos);
-
-    auto& bonuses = world.getBonuses();
-    auto& troopers = world.getTroopers();
-    auto cells = world.getCells();
-    vector< Trooper > teammates;
-    vector< Trooper > enemies;
-    for (auto& trooper : troopers) {
-        if (trooper.isTeammate()) {
-            cells[trooper.getX()][trooper.getY()] = HIGH_COVER;
-            teammates.push_back(trooper);
-        }
-        else {
-            enemies.push_back(trooper);
-        }
-    }
-    logId("we see " << enemies.size() << " enemies");
-
-    Dijkstra dijkstra(pos, cells);
-
-    for (auto& enemy : enemies) {
-        if (world.isVisible(self.getShootingRange(),
-                    self.getX(), self.getY(), self.getStance(),
-                    enemy.getX(), enemy.getY(), enemy.getStance())) {
-            Point e(enemy);
-            logId("seeing enemy at " << e);
-            if (dijkstra.reached[e.x][e.y]) {
-                target = dijkstra.next(pos, e);
-            }
-            else {
-                target = enemy;
-            }
-            logId("target = " << target << " dist = " << dijkstra.dist[target.x][target.y]);
-        }
-    }
-
-    if (self.getType() == FIELD_MEDIC &&
-            action_points >= game.getFieldMedicHealCost()) {
-        Trooper to_heal = self;
-        for (auto& t : teammates) {
-            if (pos.has_neigh(Point(t)) &&
-                    t.getHitpoints() < to_heal.getHitpoints()) {
-                to_heal = t;
-            }
-        }
-        if (to_heal.getHitpoints() < self.getMaximalHitpoints()) {
-            action.setAction(HEAL);
-            action.setX(to_heal.getX());
-            action.setY(to_heal.getY());
-            logId("heal " << to_heal.getType() << " (" << to_heal.getHitpoints() << ") at " << Point(to_heal));
-            return;
-        }
-    }
-
-    for (auto& enemy : enemies) {
-        if (world.isVisible(self.getShootingRange(),
-                    self.getX(), self.getY(), self.getStance(),
-                    enemy.getX(), enemy.getY(), enemy.getStance())) {
-            if (self.isHoldingFieldRation() &&
-                    action_points >= game.getFieldRationEatCost()) {
-                action.setAction(EAT_FIELD_RATION);
-                logId("eat field ration");
-                return;
-            }
-            if (self.isHoldingMedikit() &&
-                    action_points >= game.getMedikitUseCost()) {
-                // TODO: heal a teammate
-                action.setAction(USE_MEDIKIT);
-                action.setDirection(CURRENT_POINT);
-                logId("use medkit");
-                return;
-            }
-            // TODO: grenade
-            if (action_points >= self.getShootCost()) {
-                action.setAction(SHOOT);
-                action.setX(enemy.getX());
-                action.setY(enemy.getY());
-                logId("shoot at " << Point(action.getX(), action.getY()));
-                return;
-            }
-        }
-    }
-
-    if (action_points < game.getStandingMoveCost()) {
-        action.setAction(END_TURN);
-        logId("accumulate points");
-        return;
-    }
-
-    logId("target = " << target << " dist = " << dijkstra.dist[target.x][target.y]);
-    if (move_index == 1 || target == pos) {
-        target = dijkstra.find_reachable(Point(random(sizeX), random(sizeY)));
-        for (auto& bonus : bonuses) {
-            Point b(bonus);
-            if (
-                    (bonus.getType() == GRENADE && self.isHoldingGrenade()) ||
-                    (bonus.getType() == MEDIKIT && self.isHoldingMedikit()) ||
-                    (bonus.getType() == FIELD_RATION && self.isHoldingFieldRation())
-               ) {
-                continue;
-            }
-            if (dijkstra.dist[b.x][b.y] < dijkstra.dist[target.x][target.y]) {
-                target = b;
-            }
-        }
-        logId("target = " << target << " dist = " << dijkstra.dist[target.x][target.y]);
-    }
-
-    Point new_pos = dijkstra.next(pos, dijkstra.find_reachable(target));
-    action.setAction(MOVE);
-    action.setX(new_pos.x);
-    action.setY(new_pos.y);
-    logId("move from " << pos << " to " << new_pos);
+    static SlavaStrategy strategy;
+    strategy.move(self, world, game, action);
 }
